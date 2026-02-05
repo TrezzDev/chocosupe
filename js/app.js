5df036af17d76da979def1133f0c447796cca302
TrezzDev <githubb1122@gmail.com>
2026-02-06 06:53:53 +0700

    Uncomment and add new routes in app.js

--- js/app.js
+++ js/app.js
@@ -2,34 +2,31 @@ import { router } from './components/router.js';
 import { homePage } from './pages/home.js';
 import { animeDetailPage } from './pages/anime-detail.js';
 import { searchPage } from './pages/search.js';
-
-// Import other pages (create these as needed)
-// import { mangaDetailPage } from './pages/manga-detail.js';
-// import { characterPage } from './pages/character.js';
-// import { peoplePage } from './pages/people.js';
-// import { topPage } from './pages/top.js';
-// import { seasonPage } from './pages/season.js';
-// import { schedulePage } from './pages/schedule.js';
-// import { watchPage } from './pages/watch.js';
-// import { genrePage } from './pages/genre.js';
-// import { randomPage } from './pages/random.js';
+import { mangaDetailPage } from './pages/manga-detail.js';
+import { characterPage } from './pages/character.js';
+import { peoplePage } from './pages/people.js';
+import { topPage } from './pages/top.js';
+import { seasonPage } from './pages/season.js';
+import { schedulePage } from './pages/schedule.js';
+import { watchPage } from './pages/watch.js';
+import { genrePage } from './pages/genre.js';
+import { randomPage } from './pages/random.js';
 
 // Initialize router
 function initRouter() {
   router
     .on('/', homePage)
     .on('/anime/:id', animeDetailPage)
+    .on('/manga/:id', mangaDetailPage)
+    .on('/character/:id', characterPage)
+    .on('/people/:id', peoplePage)
     .on('/search', searchPage)
-    // Add more routes as you create the pages
-    // .on('/manga/:id', mangaDetailPage)
-    // .on('/character/:id', characterPage)
-    // .on('/people/:id', peoplePage)
-    // .on('/top/:type', topPage)
-    // .on('/season', seasonPage)
-    // .on('/schedule', schedulePage)
-    // .on('/watch', watchPage)
-    // .on('/genre/:id', genrePage)
-    // .on('/random', randomPage)
+    .on('/top/:type', topPage)
+    .on('/season', seasonPage)
+    .on('/schedule', schedulePage)
+    .on('/watch', watchPage)
+    .on('/genre/:id', genrePage)
+    .on('/random', randomPage)
     .on('/404', () => {
       document.getElementById('app').innerHTML = `
         <div class="container" style="text-align: center; padding: 100px 20px;">
